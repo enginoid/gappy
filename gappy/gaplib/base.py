58354aa891a658a26a22f14e65f01d978d48bfad
Fred Jonsson <fridrik@pyth.net>
2012-12-02 16:10:45 +0000

    Add `Solution` class.

--- gappy/gaplib/base.py
+++ gappy/gaplib/base.py
@@ -3,3 +3,9 @@ class Agent(object):
         self.capacity = capacity
         self.work_units = work_units
         self.costs = costs
+
+
+class Solution(object):
+    def __init__(self, agents, assignments):
+        self.agents = agents
+        self.assignments = assignments
--- gappy/gaplib/tests.py
+++ gappy/gaplib/tests.py
@@ -1,13 +1,32 @@
 import unittest
-from gappy.gaplib.base import Agent
+from gappy.gaplib.base import Agent, Solution
+
+get_poor_agent = lambda: Agent(capacity=5, work_units=(5, 10), costs=(2, 6))
+get_good_agent = lambda: Agent(capacity=25, work_units=(10, 20), costs=(1, 3))
 
 
 class AgentTest(unittest.TestCase):
 
     def setUp(self):
-        self.poorAgent = Agent(capacity=5, work_units=(5, 10), costs=(2, 6))
+        self.poorAgent = get_poor_agent()
+        self.goodAgent = get_good_agent()
 
     def testConstruction(self):
         self.assertEqual(self.poorAgent.capacity, 5)
         self.assertEqual(self.poorAgent.work_units[1], 10)
         self.assertEqual(self.poorAgent.costs[1], 6)
+
+
+class SolutionTest(unittest.TestCase):
+
+    def setUp(self):
+        agents = (get_good_agent(), get_poor_agent())
+
+        self.poorAgentSolution = Solution(
+            agents=agents, assignments=(0, 0))
+
+        self.goodAgentSolution = Solution(
+            agents=agents, assignments=(1, 1))
+
+    def testConstruction(self):
+        self.assertEqual(self.goodAgentSolution.assignments[1], 1)
